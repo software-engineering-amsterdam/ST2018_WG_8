695024f31551c8e23a4af2544878f184409b2fd1
Andrea van den Hooff <andrea@vandenhooff.name>
2018-10-04 11:02:02 +0200

    Bla

--- Andrea/Lab5.hs
+++ Andrea/Lab5.hs
@@ -13,8 +13,28 @@ import System.Random
 -}
 
 
--- 1 hour (incl. sudoku solution on paper)
--- Code is in Lecture5.hs
+
+{-
+    1 hour (incl. sudoku solution on paper)
+    Code is also in Lecture5.hs
+
+    nrcBlocks :: [[Int]]
+    nrcBlocks = [[2..4],[6..8]]
+
+    nrcBl :: Int -> [Int]
+    nrcBl x = concat $ filter (elem x) nrcBlocks 
+
+    freeInNrcgrid :: Sudoku -> (Row,Column) -> [Value]
+    freeInNrcgrid s (r,c) = freeInSeq (nrcGrid s (r,c))
+
+    nrcInjective :: Sudoku -> (Row, Column) -> Bool
+    nrcInjective s (r,c) = injective vs where 
+        vs = filter (/= 0) (nrcGrid s (r,c))
+
+    prune::
+    | sameNrcblock (r,c) (x,y) = 
+        (x,y,zs\\[v]) : prune (r,c,v) rest
+-}
 
 problem1 :: Grid
 problem1 = [[0,0,0,3,0,0,0,0,0],
@@ -38,34 +58,11 @@ solution1 = [[4,7,8,3,9,2,6,1,5],
              [9,8,3,1,4,7,5,6,2],
              [1,4,2,5,6,3,8,9,7]]
 {-
-    Exercise 2:
-
-    In order to refactor the code such that the formulation of constraints
-    becomes more uniform, the following definitions were proposed:
-
-    > type Position = (Row,Column)
-    > type Constrnt = [[Position]]
-
-    The regular constraints for Sudoku can now be stated as:
-
-    > rowConstrnt = [[(r,c)| c <- values ] | r <- values ]
-    > columnConstrnt = [[(r,c)| r <- values ] | c <- values ]
-    > blockConstrnt = [[(r,c)| r <- b1, c <- b2 ] | b1 <- blocks, b2 <- blocks ]
-
-    The generation of the values that are still possible at a given position now
-    takes the following shape:
-
-    > freeAtPos' :: Sudoku -> Position -> Constrnt -> [Value]
-    > freeAtPos' s (r,c) xs = let
-    >    ys = filter (elem (r,c)) xs
-    >  in
-    >    foldl1 intersect (map ((values \\) . map s) ys)
-
-    Refactor the code along the lines of this proposal, and next compare the two
-    versions for extendability and efficiency. Which of the two versions is easier
-    to modify for NRC sudokus, and why? Which of the two versions is more efficient?
+    Exercise 2: Refactor the code along the lines of this proposal,
+    and next compare the two versions for extendability and efficiency.
+    Which of the two versions is easier to modify for NRC sudokus,
+    and why? Which of the two versions is more efficient?
     Devise your own testing method for this, and write a short test report.
-
     Deliverables: Refactored code, test report, indication of time spent.
 -}
 
