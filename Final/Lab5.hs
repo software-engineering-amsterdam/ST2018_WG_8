611d8ed8f6c6900918a54a5cd4a932c3ed576f2d
annavalachi <42973026+annavalachi@users.noreply.github.com>
2018-10-09 23:51:40 +0200

    Update Lab5.hs

--- Final/Lab5.hs
+++ Final/Lab5.hs
@@ -25,71 +25,9 @@ import Lecture5
 -}
 
 {-
-    Exercise 5:
-
-    Extend the code of the lectures to create a program that generates NRC
-    Sudoku problems, that is, Sudoku problems satisfying the extra constraint
-    explained in the NRC exercise above.
-
+    Exercise 5 is in Lecture5Exercise5.hs
     Deliverables: NRC Sudoku generator, indication of time spent.
 -}
-
--- I created a prime functions for all functions that use the prune function
--- and Anna added the constraint for NRC sudokus to the prune function.
--- The generator will call these functions eventually and instead of generate
--- a regular sudoku, it will generate one that adheres to the NRC constraints.
-prune' :: (Row,Column,Value) -> [Constraint] -> [Constraint]
-prune' _ [] = []
-prune' (r,c,v) ((x,y,zs):rest)
-    | r == x = (x,y,zs\\[v]) : prune' (r,c,v) rest
-    | c == y = (x,y,zs\\[v]) : prune' (r,c,v) rest
-    | sameblock (r,c) (x,y) =
-        (x,y,zs\\[v]) : prune' (r,c,v) rest
-    | newsameblock (r,c) (x,y) =
-        (x,y,zs\\[v]) : prune' (r,c,v) rest--added/ checks newsameblock
-    | otherwise = (x,y,zs) : prune' (r,c,v) rest
-
-search' :: (node -> [node])
-       -> (node -> Bool) -> [node] -> [node]
-search' children goal [] = []
-search' children goal (x:xs)
-  | goal x    = x : search' children goal xs
-  | otherwise = search' children goal ((children x) ++ xs)
-
-solveNs' :: [Node] -> [Node]
-solveNs' = search' succNode' solved
-
-succNode' :: Node -> [Node]
-succNode' (s,[]) = []
-succNode' (s,p:ps) = extendNode' (s,ps) p
-
-extendNode' :: Node -> Constraint -> [Node]
-extendNode' (s,constraints) (r,c,vs) =
-   [(extend s ((r,c),v),
-     sortBy length3rd $
-         prune' (r,c,v) constraints) | v <- vs ]
-
-
-rsuccNode' :: Node -> IO [Node]
-rsuccNode' (s,cs) = do
-    xs <- getRandomCnstr cs
-    if null xs then return [] else return (extendNode' (s,cs\\xs) (head xs))
-
-rsolveNs' :: [Node] -> IO [Node]
-rsolveNs' ns = rsearch rsuccNode' solved (return ns)
-
--- This generates a solved sudoku NRC style
-genRandomNRCSudoku :: IO Node
-genRandomNRCSudoku = do
-    [r] <- rsolveNs' [emptyN]
-    return r
-
--- This function minimizes the solved sudoku in order to make it a sudoku puzzle.
-genNRCProblem :: IO Node
-genNRCProblem = do
-    x <- genRandomNRCSudoku
-    x' <- genProblem x
-    return x'
 
 {-
     Exercise 6 (Bonus): Can you find a way of classifying the difficulty of a Sudoku problem? 
